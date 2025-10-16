7140c65ebcb4d8e2474fe8fabf746cb83711c0a1
OskarYnov <oskar.morel@ynov.com>
2025-10-16 20:48:56 +0200

    CRUD Users (pas testé j'ai pas JWT donc jprend un 403)

--- api/src/main/java/com/api/hackathon/config/SecurityConfig.java
+++ api/src/main/java/com/api/hackathon/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/users/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
