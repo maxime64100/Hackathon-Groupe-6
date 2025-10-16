e41f2393e4e2683eef4aa2241c6e29955c22a38b
maxime64100 <maximefallek1@gmail.com>
2025-10-16 18:02:01 +0200

    add dasboard front only

--- SoukSoccer/src/app/app.routes.ts
+++ SoukSoccer/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {Reservation} from './reservation/reservation';
+import {Dashboard} from './dashboard/dashboard';
 
 export const routes: Routes = [
-  {path: 'reservation', component: Reservation}
+  {path: 'reservation', component: Reservation},
+  {path: 'dashboard', component: Dashboard}
 ];
